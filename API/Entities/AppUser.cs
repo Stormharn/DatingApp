ecd878d1e06e09547b9c0df0538437e257cfad0e
Allen Sparks <asparks@protonmail.com>
2022-08-12 22:51:40 -0500

    Basic Authentication / EOS 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,4 +8,8 @@ public class AppUser
 {
     public int ID { get; set; }
     public string UserName { get; set; }
+
+    public byte[] PasswordHash {get; set; }
+
+    public byte[] PasswordSalt { get; set; }
 }
