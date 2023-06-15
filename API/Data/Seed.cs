d53400d478d9e7da015b81e6c561f1fe67a58506
Allen Sparks <asparks@protonmail.com>
2023-06-15 15:32:15 -0500

    Initial production build

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,6 +9,12 @@ namespace API.Data
 {
     public class Seed
     {
+        public static async Task ClearConnections(DataContext context)
+        {
+            context.Connections.RemoveRange(context.Connections);
+            await context.SaveChangesAsync();
+        }
+        
         public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) { return; }
@@ -34,13 +40,21 @@ namespace API.Data
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
+                user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+                user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
             }
 
             var admin = new AppUser
             {
-                UserName = "admin"
+                UserName = "admin",
+                KnownAs = "admin",
+                Created = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc),
+                LastActive = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc),
+                DateOfBirth = DateOnly.MinValue,
+                Gender = "any",
+                InterestedIn = "any"
             };
 
             await userManager.CreateAsync(admin, "Admin@1234");
